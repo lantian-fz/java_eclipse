febbe8c2e81f202c54fdf16c3048d8378fe7c382
lantian-fz <2459396922@qq.com>
2019-09-06 11:10:28 +0800

    练习代码

---
+++ test_2/src/test_2/test.java
@@ -0,0 +1,28 @@
+package test_2;
+
+public class test {
+	public static void main(String[] args) {
+		int a=1;
+		int b=3;
+		
+//		a=b++;
+		System.out.println(a&b);
+		System.out.println(b);
+		
+//		if(a>b)
+//			System.out.println("a = "+a);
+//		else
+//			System.out.println("b = "+b);
+////		System.out.println(byte);
+//		float c=3.14f;
+//		System.out.println(c);
+//		boolean var=true;
+//		System.out.println(var);
+//		String str="hello world!";
+//		System.out.println(str);
+////		int x;
+////		System.out.println(x);
+//		String arr="hhh";
+//		System.out.println(str+arr);
+	}
+}
