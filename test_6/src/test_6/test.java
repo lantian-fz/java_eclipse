f3615b2ace5b5822addecad6ed168f5a17deeb02
lantian-fz <2459396922@qq.com>
2019-09-08 20:43:35 +0800

    练习代码

---
+++ test_6/src/test_6/test.java
@@ -0,0 +1,16 @@
+package test_6;
+//输出乘法口诀表
+public class test {
+	public static void main(String[] args)
+	{
+		int i=0;
+		int j=0;
+		
+		for(i=1;i<=9;i++)
+		{
+			for(j=1;j<=i;j++)
+				System.out.print(j+"*"+i+" = "+i*j+" ");
+			System.out.println();
+		}
+	}
+}
