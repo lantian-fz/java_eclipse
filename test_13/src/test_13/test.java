e29ad389da20c5083389ecf31acf9f59be6ddfd1
lantian-fz <2459396922@qq.com>
2019-09-23 23:32:16 +0800

    练习代码

--- test_13/src/test_13/test.java
+++ test_13/src/test_13/test.java
@@ -12,7 +12,18 @@ public class test {
 		System.out.println(stu1.age);
 		
 		stu1.call(stu1.name);
-		
 		stu1.eat();
+		
+		System.out.println("++++++++++++++++++++++++");
+		Student stu2=stu1;
+		stu2.name = "öï";
+		stu2.age = 20;
+		System.out.println(stu2.name);
+		System.out.println(stu2.age);
+		stu2.call(stu2.name);
+		stu1.name="µÄ";
+		System.out.println(stu2.name);
+		stu2.name="Óê";
+		System.out.println(stu1.name);
 	}
 }
