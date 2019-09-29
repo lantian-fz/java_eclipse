2c04379f6dd5652a452464239b667805b3d2ea11
lantian-fz <2459396922@qq.com>
2019-09-29 23:36:56 +0800

    练习代码

---
+++ test_16/src/test_16/test.java
@@ -0,0 +1,12 @@
+package test_16;
+
+public class test {
+
+	public static void main(String[] args) {
+		Student stu = new Student();
+		
+		stu.setName("À¶Ìì");
+		stu.setAge(21);
+		System.out.println("ĞÕÃû£º"+stu.getName()+"£¬ÄêÁä£º"+stu.getAge());
+	}
+}
